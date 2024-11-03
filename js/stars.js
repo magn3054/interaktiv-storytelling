3ce694121e6ded34d094bb213ea60f6876b0dfde
magn3054 <mdch20@student.aau.dk>
2024-11-03 11:59:52 +0100

    optimering

--- js/stars.js
+++ js/stars.js
@@ -1,11 +1,11 @@
-const main = document.querySelector('main');
+const body = document.querySelector('body');
 const circleCount = 100; // antal stjerner
 
 for (let i = 0; i < circleCount; i++) {
     const circle = document.createElement('div');
     circle.classList.add('circle');
-    const line = document.createElement('div');
-    line.classList.add('line');
+    // const line = document.createElement('div');
+    // line.classList.add('line');
     
     // Giver et random tal mellem 2 og 5 til størrelsen af stjernen
     const size = Math.random() * 4 + 1;
@@ -19,13 +19,13 @@ for (let i = 0; i < circleCount; i++) {
     let y = `${Math.random() * 100}`;
     circle.style.top = `${x}%`;
     circle.style.left = `${y}%`;
-    line.style.top = `${x}%`;
-    line.style.left = `${y}%`;
+    // line.style.top = `${x}%`;
+    // line.style.left = `${y}%`;
 
     const delay = Math.random() * 5; // giv et random delay til animationen af stjernen
     circle.style.animationDelay = `${delay}s`;
-    line.style.animationDelay = `${delay}s`;
+    // line.style.animationDelay = `${delay}s`;
     
-    main.appendChild(circle);
-    main.appendChild(line);
+    body.appendChild(circle);
+    // body.appendChild(line);
 }

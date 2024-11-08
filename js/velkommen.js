a0a348f957c6b0fa2cc1a4854eb21201ac5e2f52
MieZilmer <158352685+MieZilmer@users.noreply.github.com>
2024-11-08 16:45:22 +0100

    Velkommen med astra
    Velkommen side, hvor hun vinker og siger ud i rummet

---
+++ js/velkommen.js
@@ -0,0 +1,18 @@
+"use strict;"
+
+
+
+const audio = new Audio();
+const mund = document.getElementById("info-astra");
+
+function handleClick() {
+  document.querySelector('.velkommen-vink').style.display = 'none';
+  document.getElementById('velkommen-tale').style.display = 'block';
+  mund.src = "/img/talking-gif.gif"; // Skift fra vink til gif af Astra der taler
+  audio.src = "/audio/velkommen.mp3"; // afspiller lyden fra velkommen.mp3
+  audio.play();
+  audio.onended = function() {
+      mund.src = "/img/talking-still.png";   // skifter billedet tilbage til still når hun har snakket
+  };
+}
+
